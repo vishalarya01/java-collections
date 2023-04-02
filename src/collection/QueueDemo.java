eed3486bae53a30d3a7817c3b74953b844c8e549
vishal arya <vishalarya1701@gmail.com>
2023-04-02 22:16:28 +0530

    QueueDemo

--- src/collection/QueueDemo.java
+++ src/collection/QueueDemo.java
@@ -1,2 +1,29 @@
-package collection;public class QueueDemo {
+package collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+
+    public static void main(String[] args) {
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+        queue.offer(40);
+
+        System.out.println(queue.peek());
+
+//        remove
+        System.out.println(queue.poll());
+
+        System.out.println(queue.peek());
+
+        System.out.println(queue.isEmpty());
+
+
+
+    }
 }
