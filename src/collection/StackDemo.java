50e5c28a1d7859f59160e1f959fe9e143bc9c517
vishal arya <vishalarya1701@gmail.com>
2023-04-02 22:19:25 +0530

    QueueDemo

---
+++ src/collection/StackDemo.java
@@ -0,0 +1,22 @@
+package collection;
+
+import java.util.Stack;
+
+public class StackDemo {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+
+        stack.push(3);
+
+        while (!stack.isEmpty()){
+            System.out.println(stack.peek());
+            stack.pop();
+    }
+
+    }
+}
